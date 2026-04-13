1be9d53b86d2a38005c320c56f385fe616701c3e
xuesong.lei <228389787@qq.com>
2026-04-13 23:04:25 +0800

    feat: 移动端添加友链页面链接到导航菜单

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -39,6 +39,7 @@
             <router-link to="/category/-1" @click.native="mobileOpen = false"><i class="el-icon-folder-opened" /> 分类</router-link>
             <router-link to="/tag/-1" @click.native="mobileOpen = false"><i class="el-icon-price-tag" /> 标签</router-link>
             <router-link to="/archive" @click.native="mobileOpen = false"><i class="el-icon-collection" /> 归档</router-link>
+            <router-link to="/links" @click.native="mobileOpen = false"><i class="el-icon-link" /> 友链</router-link>
             <router-link to="/about" @click.native="mobileOpen = false"><i class="el-icon-user" /> 关于我</router-link>
           </div>
           <form class="mobile-search" @submit.prevent="handleSearch(mobileKeyword)">
